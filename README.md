ff3cafbf65f900cee2e1ea8c4376d47cbd9f048f
lzhengms <2393408826@qq.com>
2019-08-06 19:50:32 +0800

    'ts'
    Change-Id: I64124c64a1b9243adf6ad3d92c0284afbd42ce3c

---
+++ README.md
@@ -0,0 +1,5 @@
+1.运行：
+npm  i
+npm run build
+
+2.直接访问index.html文件即可
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: __dirname + "/index.tsx",
+  output: {
+      filename: "bundle.js",
+      path: __dirname + "/dist"
+  },
+  devtool: "soruce-map",
+  mode: "development",
+  resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+      rules: [
+          {test: /\.tsx?/, loader: 'awesome-typescript-loader'},
+          {test: /\.js/, enforce: 'pre', loader: 'source-map-loader'}
+      ]
+  },
+  externals: {
+      "react": "React",
+       "react-dom": "ReactDOM"
+  }
+
+}
